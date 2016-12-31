4c81fd3ba6af508f01c3b6344bc356fe1edc55ab
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2016-12-31 14:14:12 +0300

    Add files via upload

---
+++ DownloadedFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dropbox.Api.Files;
+using Dropbox.Api.Users;
+
+namespace BelkaCloudDownloader.Dropbox
+{
+	//using global::Google.Apis.Drive.v3.Data;
+
+	/// <summary>
+	/// Represents a file that was successfully downloaded from Google Drive.
+	/// </summary>
+	public sealed class DownloadedFile
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DownloadedFile"/> class.
+		/// </summary>
+		/// <param name="metaInformation">Metainformation about file as it was returned by Google server.</param>
+		/// <param name="path">A path to a downloaded file on a local file system.</param>
+		public DownloadedFile(Account metaInformation, string path)
+		{
+			this.MetaInformation = metaInformation;
+			this.Path = path;
+		}
+
+		/// <summary>
+		/// Gets metainformation about file as it was returned by Google server.
+		/// </summary>
+		public Account MetaInformation { get; }
+
+		/// <summary>
+		/// Gets a path to a downloaded file on a local file system.
+		/// </summary>
+		public string Path { get; }
+	}
+}
---
+++ DriveDownloaderSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BelkaCloudDownloader.Dropbox
+{
+	public sealed class DriveDownloaderSettings
+	{
+		/// <summary>
+		/// Gets or sets valid and non-expired Refresh Token from Google Drive Sync.
+		/// </summary>
+		public string RefreshToken { get; set; }
+
+		/// <summary>
+		/// Gets or sets directory where we shall store downloaded files.
+		/// </summary>
+		public string TargetDirectory { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether to download files from cloud or only meta-information.
+		/// </summary>
+		public bool DoDownloadFiles { get; set; }
+	}
+}
