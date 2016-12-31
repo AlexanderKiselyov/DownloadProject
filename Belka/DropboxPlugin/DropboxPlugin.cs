5c6cf9f48d75fb69901ff3b4fc167a435823baac
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2016-12-31 14:16:49 +0300

    Add files via upload

---
+++ Belka/DropboxPlugin/DropboxPlugin.cs
@@ -0,0 +1,14 @@
+﻿namespace BelkaCloudDownloader.Gui
+{
+	using System.Collections.Generic;
+
+	/// <summary>
+	/// Dropbox support.
+	/// </summary>
+	internal sealed class DropboxPlugin : CloudPlugin
+	{
+		public IList<Protocol> Protocols { get; } = new List<Protocol> { new DropboxProtocol() };
+
+		public string CloudName { get; } = "Dropbox";
+	}
+}
